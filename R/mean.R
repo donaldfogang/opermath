02386956ae145711c9b700429256b2f06188d1ff
Donald Fogang <donaldfogang3@gmail.com>
2025-12-19 22:40:18 +0100

    Your message

--- R/mean.R
+++ R/mean.R
@@ -6,7 +6,7 @@
 #' @return Numeric scalar: the mean of x excluding NA values.
 #' @examples
 #' mean_vector(c(1, 5, 3))
-#' mean_vector(c(1, 2, NA))
+#' mean_vector(c(10, 2, NA))
 #' @export
 mean_vector <- function(x) {
   if (!is.numeric(x)) stop("x must be numeric")
--- R/sum.R
+++ R/sum.R
@@ -5,7 +5,7 @@
 #' @param x Numeric vector.
 #' @return Numeric scalar: the sum of x.
 #' @examples
-#' sum_vector(c(1, 2, 3))
+#' sum_vector(c(9, 2, 3))
 #' sum_vector(c(NA, 7, 2))  # returns NA if NA present
 #' @export
 sum_vector <- function(x) {
